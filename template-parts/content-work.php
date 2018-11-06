8c32a83e36a69db0ce6c137ab40d4af002ec23de
Ko Takagi <knockoutmarch@gmail.com>
2018-11-07 08:46:37 +0900

    update single page

--- template-parts/content-work.php
+++ template-parts/content-work.php
@@ -3,4 +3,16 @@
 	global $wp_embed;
 	echo $wp_embed->run_shortcode( "[embed]" . tscfp( 'tweet' ) . '[/embed]' );
 	?>
+
+	<?php
+	if ( is_single() ) :
+	?>
+	<ul>
+		<li>棋士：<?php echo get_the_term_list( get_the_ID(), 'player', '', ', ' ); ?></li>
+		<li>作者：<?php echo get_the_term_list( get_the_ID(), 'artist', '', ', ' ); ?></li>
+	</ul>
+	<?php
+	endif;
+	?>
 </div>
+
